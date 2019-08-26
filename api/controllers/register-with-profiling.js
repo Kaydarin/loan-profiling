ab502f0e831024c87b8f5c3945431b293c2ed37a
Kaydarin <zim50_killerz@yahoo.com>
2019-08-26 10:23:52 +0800

    Finalize API with comments

--- api/controllers/register-with-profiling.js
+++ api/controllers/register-with-profiling.js
@@ -36,15 +36,24 @@ module.exports = {
   },
 
   fn: async function(inputs, exits) {
+    /*
+      -- Calculate user profile score
+    */
     const userProfilingRes = await sails.helpers.calculateUserProfileScore(
       inputs.savingAns,
       inputs.loanAns
     );
 
+    // Return error if calculation fails
+    // due to mismatch input data
     if (userProfilingRes.statusCode === 204) {
       return exits.badRequest();
     }
 
+    /*
+      -- Create user along with their score or,
+      -- update user score
+    */
     Users.findOrCreate(
       { email: inputs.email },
       {
@@ -55,10 +64,13 @@ module.exports = {
     ).exec(async (err, user, wasCreated) => {
       let updateOrCreateMessage;
 
+      // If something wrong with the creation,
+      // return error
       if (err) {
         return this.res.serverError(err);
       }
 
+      // If successfully created, return success message
       if (wasCreated) {
         updateOrCreateMessage = 'Created user, ' + user.name;
       } else {
@@ -66,9 +78,11 @@ module.exports = {
           profileScore: userProfilingRes.totalScore
         });
 
+        // If successfully created, return success message
         updateOrCreateMessage = 'Updated ' + user.name + ' profile score';
       }
 
+      // Return success response
       return exits.success({
         savings: userProfilingRes.savings,
         loan: userProfilingRes.loan,
